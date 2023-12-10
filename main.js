9ab983671256538613444f37f71cccf55ca4a174
Kariakito <pilarmaestre@gmail.com>
2023-12-10 18:57:41 +0000

    finish lesson 9 and 10

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            selectedVariant: 0,
-            details: [ '50% algodon', '30% lana', '20% polyester' ],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 20, onSale: false },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: true },
-            ],
-            sizes: [ 5, 6, 7, 8 ,9],
+            cart: [],
+            premium: true
         }
     },
     methods: {
-        addToCart() {
-            this.cart += 1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-            console.log(index)
-        },
-    },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        sale() {
-            return this.brand + ' ' + this.variants[this.selectedVariant].onSale + ' is on sale'
+        updateCart(id) {
+            this.cart.push(id)
         }
     }
 })
