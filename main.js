2961a7544ea6904a593a55dbc47096e9341e89f3
Kariakito <pilarmaestre@gmail.com>
2023-12-10 15:04:26 +0000

    code challenge 3 all done and working

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
         product: 'Socks, ahora si se ve todo lo q yop quier q se vea no joda !!!! att Pilar',
         description: 'arcoires y unicornios bling bling',
-        image: './assets/images/socks_green.jpg'
+        image: './assets/images/socks_green.jpg',
+        url: 'https://img.freepik.com/free-vector/cute-unicorn-drinking-boba-milk-tea-with-rainbow-cartoon-vector-icon-illustration-animal-drink-icon_138676-7412.jpg?size=626&ext=jpg'
         }
     }
 })
