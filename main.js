dab2d9ce58cd172b76a512c32c885b5efbb8ae1b
Kariakito <pilarmaestre@gmail.com>
2023-12-10 16:18:13 +0000

    finish lesson 6

--- main.js
+++ main.js
@@ -11,10 +11,18 @@ const app = Vue.createApp({
             inStock: true,
             details: [ '50% algodon', '30% lana', '20% polyester' ],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             sizes: [ 5, 6, 7, 8 ,9],
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
         }
     }
 })
