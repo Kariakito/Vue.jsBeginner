4ee4512dfe3f9dfd802c56e030e09b6bae786eb1
Kariakito <pilarmaestre@gmail.com>
2023-12-10 15:37:18 +0000

    start lesson 5

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const app = Vue.createApp({
         url: 'https://img.freepik.com/free-vector/cute-unicorn-drinking-boba-milk-tea-with-rainbow-cartoon-vector-icon-illustration-animal-drink-icon_138676-7412.jpg?size=626&ext=jpg',
         inventory: 2,
         onSale: false,
+        inStock: true,
         }
     }
 })
