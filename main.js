b1d1d3e431236b455dd7d39af0d24b00314285d9
Kariakito <pilarmaestre@gmail.com>
2023-12-10 14:27:36 +0000

    code challenge 1 add description and is pint in the browser

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-        product: 'Socks, ahora si se ve todo lo q yop quier q se vea no joda !!!! att Pilar'
+        product: 'Socks, ahora si se ve todo lo q yop quier q se vea no joda !!!! att Pilar',
+        description: 'arcoires y unicornios bling bling'
         }
     }
 })
