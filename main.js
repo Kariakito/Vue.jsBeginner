c2088137122eda03338a8a1b62b4c7eeed69fed0
Kariakito <pilarmaestre@gmail.com>
2023-12-10 14:46:43 +0000

    image is showing in browser

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data() {
         return {
         product: 'Socks, ahora si se ve todo lo q yop quier q se vea no joda !!!! att Pilar',
-        description: 'arcoires y unicornios bling bling'
+        description: 'arcoires y unicornios bling bling',
+        image: './assets/images/socks_green.jpg'
         }
     }
 })
